9a4dc0d241b4854a96967361665a7418e38e2f42
Goldenapple3619 <gandonsteven@gmail.com>
2026-03-16 14:52:02 +0100

    Fix build

---
+++ scripts/build_windows-i386.bat
@@ -0,0 +1,15 @@
+mkdir build/windows-i386
+cd build/windows-i386
+
+cmake ../.. \
+    -DCMAKE_TOOLCHAIN_FILE=../mingw_toolchain-i386.cmake \
+    -DDIST_DIR=../../dist/windows-i386 \
+    -DSDL2_IMAGE=ON \
+    -DSDL2_MIXER=ON \
+    -DSDL2_TTF=ON \
+    -DCMAKE_C_FLAGS="-m32" \
+    -DCMAKE_CXX_FLAGS="-m32" \
+    -DTARGET_ARCH="i386" \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.20
+
+cmake --build . -- -j8
