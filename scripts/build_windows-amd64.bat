5ce7da6e2a5a341164524791251e6fbbd51447eb
Goldenapple3619 <gandonsteven@gmail.com>
2026-03-16 14:56:23 +0100

    Fix build

--- scripts/build_windows-amd64.bat
+++ scripts/build_windows-amd64.bat
@@ -1,14 +1,6 @@
-mkdir build/windows-amd64
-cd build/windows-amd64
+mkdir -p build\windows-amd64
+cd build\windows-amd64
 
-cmake ../.. -DCMAKE_TOOLCHAIN_FILE=../mingw_toolchain-amd64.cmake \
-    -DDIST_DIR=../../dist/windows-amd64 \
-    -DSDL2_IMAGE=ON \
-    -DSDL2_MIXER=ON \
-    -DSDL2_TTF=ON \
-    -DCMAKE_C_FLAGS="-m64" \
-    -DCMAKE_CXX_FLAGS="-m64" \
-    -DTARGET_ARCH="amd64" \
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake ..\.. -DCMAKE_TOOLCHAIN_FILE=..\mingw_toolchain-amd64.cmake -DDIST_DIR=..\..\dist\windows-amd64 -DSDL2_IMAGE=ON -DSDL2_MIXER=ON -DSDL2_TTF=ON -DCMAKE_C_FLAGS="-m64" -DCMAKE_CXX_FLAGS="-m64" -DTARGET_ARCH="amd64" -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 
 cmake --build . -- -j8
