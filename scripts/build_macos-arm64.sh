80a5281921106073b60f0db5bd189cef1c13352b
Goldenapple3619 <gandonsteven@gmail.com>
2026-03-31 00:29:10 +0200

    Edit github workflow

--- scripts/build_macos-arm64.sh
+++ scripts/build_macos-arm64.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -e
 
-mkdir -p build/macos-amd64
-cd build/macos-amd64
+mkdir -p build/macos-arm64
+cd build/macos-arm64
 
 cmake ../.. \
-    -DDIST_DIR=../../dist/macos-amd64 \
+    -DDIST_DIR=../../dist/macos-arm64 \
     -DSDL2_IMAGE=ON \
     -DSDL2_MIXER=ON \
     -DSDL2_TTF=ON \
     -DCMAKE_C_FLAGS="-m64" \
     -DCMAKE_CXX_FLAGS="-m64" \
-    -DTARGET_ARCH="amd64" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 
