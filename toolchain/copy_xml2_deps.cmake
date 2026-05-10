ae342a3b656e60e3ee9b66a5262d23a43f9abe43
Goldenapple3619 <gandonsteven@gmail.com>
2026-05-10 18:19:14 +0200

    Fix compilation linux & windows

--- toolchain/copy_xml2_deps.cmake
+++ toolchain/copy_xml2_deps.cmake
@@ -1,23 +1,37 @@
 file(GET_RUNTIME_DEPENDENCIES
     LIBRARIES "${XML2_LIB}"
-    RESOLVED_DEPENDENCIES_VAR  xml2_deps
+    RESOLVED_DEPENDENCIES_VAR   xml2_deps
     UNRESOLVED_DEPENDENCIES_VAR xml2_unresolved
-    # Don't copy glibc, libm, libpthread — those are guaranteed on any Linux
-    EXCLUDE_REGEXES
-        ".*libm\\.so.*"
-        ".*libc\\.so.*"
-        ".*libpthread\\.so.*"
-        ".*libdl\\.so.*"
-        ".*ld-linux.*"
-        ".*linux-vdso.*"
 )
 
 file(MAKE_DIRECTORY "${DIST_DIR}/lib")
 
+set(SYSTEM_LIB_PATTERNS
+    ".*libm\\.so.*"
+    ".*libc\\.so.*"
+    ".*libpthread\\.so.*"
+    ".*libdl\\.so.*"
+    ".*ld-linux.*"
+    ".*linux-vdso.*"
+    ".*libgcc_s.*"
+    ".*libstdc\\+\\+.*"
+    ".*librt\\.so.*"
+)
+
 foreach(dep ${xml2_deps})
-    get_filename_component(dep_name "${dep}" NAME)
-    message(STATUS "Packing xml2 dep: ${dep_name}")
-    file(COPY "${dep}" DESTINATION "${DIST_DIR}/lib")
+    set(_skip FALSE)
+    foreach(pattern ${SYSTEM_LIB_PATTERNS})
+        if(dep MATCHES "${pattern}")
+            set(_skip TRUE)
+            break()
+        endif()
+    endforeach()
+
+    if(NOT _skip)
+        get_filename_component(dep_name "${dep}" NAME)
+        message(STATUS "Packing xml2 dep: ${dep_name}")
+        file(COPY "${dep}" DESTINATION "${DIST_DIR}/lib")
+    endif()
 endforeach()
 
 foreach(dep ${xml2_unresolved})
